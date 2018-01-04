300271f24a3266d9ac094e1556da8845660f7b8b
Jerry W <jerrywiltse@gmail.com>
2018-01-04 15:28:41 -0500

    refreshed all

--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,44 @@
-from conans import ConanFile, CMake, tools, os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
 
 class BlackholeConan(ConanFile):
-    name = "Blackhole"
+    name = "blackhole"
     version = "1.9.0"
-    description = ""
-    license = "https://github.com/3Hren/blackhole/blob/master/LICENSE"
-    url = "https://github.com/3Hren/blackhole"
+    description = "Blackhole is an attribute-based logger with strong focus on performance"
+    url = "https://github.com/bincrafters/conan-blackhole"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    lib_short_name = "blackhole"
-    archive_name = "{0}-{1}".format(lib_short_name, version)
-    requirements = "Boost.Thread/1.65.1@bincrafters/stable"
-            
+    requires = (
+        "Boost.Thread/1.65.1@bincrafters/stable", 
+        "Boost.System/1.65.1@bincrafters/stable",
+        "cmake_findboost_modular/0.1.0@bincrafters/stable"
+     )
+     
     def source(self):
-        tools.get("{0}/archive/v{1}.tar.gz".format(self.url, self.version))
-                
+        source_url = "https://github.com/3Hren/blackhole"
+        extracted_dir = "{0}-{1}".format(self.name, self.version)
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        os.rename(extracted_dir, self.source_subfolder)
+        
     def build(self):
         cmake = CMake(self)
-        conan_magic_lines = r"""project(${LIBRARY_NAME})
-    INCLUDE(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-    CONAN_BASIC_SETUP()"""
-        tools.replace_in_file(os.path.join(self.archive_name, "CMakeLists.txt"), r"project(${LIBRARY_NAME})", conan_magic_lines)
-        cmake = CMake(self)
-        cmake.configure(source_dir=self.archive_name)
+        cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
-    
+        cmake.install()
+
     def package(self):
-        include_dir = os.path.join(self.archive_name, 'include')
+        include_dir = os.path.join(self.source_subfolder, 'include')
         self.copy(pattern="*.h", dst="include", src=include_dir)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so*", dst="lib", keep_path=False)
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,7 @@
-from conans import ConanFile, CMake, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
 
@@ -10,10 +13,13 @@ class TestPackageConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        
-    def imports(self):
-        self.copy("*", dst="bin", src="bin")
-        self.copy("*", dst="bin", src="lib")
-        
+
     def test(self):
-        self.run(os.path.join("bin","test_package"))
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
